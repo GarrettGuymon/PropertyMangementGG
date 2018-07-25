6a97cc06ca50233bc063931e6aaba9f1c5d99e8d
dsd <gcguymon@gmail.com>
2018-07-25 09:48:23 -0600

    setup auth routes and headerwrapper component

---
+++ src/components/auth/signin.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class signin extends Component {
+    render() {
+        return (
+            <div className='sign-in'>
+                ...signin
+            </div>
+        )
+    }
+}
+export default signin;
---
+++ src/components/auth/signup.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class signup extends Component {
+    render() {
+        return (
+            <div className='sign-up'>
+                ...signup
+            </div>
+        )
+    }
+}
+export default signup;
